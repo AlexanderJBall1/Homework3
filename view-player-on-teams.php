925935bcf3e9adc2361a5b90e059a87963af44d7
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-12-09 14:20:48 -0600

    Update view-player-on-teams.php

--- view-player-on-teams.php
+++ view-player-on-teams.php
@@ -12,13 +12,6 @@ while ($team = $teams->fetch_assoc()){
       <h5 class="card-title"><?php echo $team['Name']; ?></h5>
       <p class="card-text">
         <ul class="list-group">
-<?php
- $playeronteams = selectPlayerOnTeams($team['Team_ID']);
-while($player = $playeronteams->fetch_assoc()){
-?>
-    <li class="list-group-item"><?php echo $player['Player_Name']; ?> - <?php echo $player['Salary']; ?> - <?php echo $player['Years']; ?></li>
-
-<?php
 include "view-player-on-teams-editform.php";
 ?>
 
